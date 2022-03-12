15cf91ec2b1c5afd98e2af8f4f73514062f007b0
ogawayuto <ogawa.yuuto.72r@st.kyoto-u.ac.jp>
2022-03-12 15:52:16 +0900

    medium_15

---
+++ problems/medium/GridRepainting2.py
@@ -0,0 +1,23 @@
+H, W = list(map(int, input().split()))
+s = [list(input()) for _ in range(H)]
+ans = 'Yes'
+black = []
+for i in range(H):
+  for j in range(W):
+    if s[i][j]=='#':
+      black.append([i,j])
+
+def paint(x,y):
+  if [x+1,y] in black or [x-1,y] in black or [x,y+1] in black or [x,y-1] in black:
+    return True
+  else: False
+
+for xy in black:
+  if paint(*xy) :
+    continue
+  else:
+    ans = 'No'
+    break
+
+print(ans)
+
---
+++ problems/medium/KEYENCEstring.py
@@ -0,0 +1,15 @@
+S = list(input())
+n=len(S)
+ans='NO'
+key = list('keyence')
+
+for i in range(n):
+  for j in range(i,n+1):
+    inst = S.copy()
+    del inst[i:j]
+    if inst == key:
+      ans = 'YES'
+      break
+
+
+print(ans)
