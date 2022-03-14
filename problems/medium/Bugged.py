1edf8e4693faef560f3d539fa62a71dac31a9d86
ogawayuto <ogawa.yuuto.72r@st.kyoto-u.ac.jp>
2022-03-14 16:06:09 +0900

    medium_18

---
+++ problems/medium/Bugged.py
@@ -0,0 +1,14 @@
+N = int(input())
+s = [int(input()) for _ in range(N)]
+ans = sum(s)
+s = [i for i in s if i % 10 != 0]
+s.sort()
+
+if ans % 10 !=0:
+  ans
+elif s :
+  ans -= s[0]
+else:
+  ans = 0
+  
+print(ans)
---
+++ problems/medium/Sentou.py
@@ -0,0 +1,11 @@
+N, T = list(map(int, input().split()))
+t = list(map(int, input().split()))
+ans = 0
+
+for i in range(N):
+  if i == N-1:
+    ans += T
+  else:
+    ans += min(T, t[i+1] - t[i])
+
+print(ans)
